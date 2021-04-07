1231a38826bf0a898f7848c82e0ba36f98f97b26
Juliana Meneguete <jumeneguete@gmail.com>
2021-04-07 19:33:56 -0300

    Jogo de cartas com n cartas virado para baixo

--- scripts/script.js
+++ scripts/script.js
@@ -7,5 +7,23 @@ while (resto !== 0 || qtdd<4 || qtdd>14){
     resto = qtdd % 2;
 }
 
-console.log("chegou aqui");
+function mostrarCartas (){
+
+    let i = 0;
+
+    while (i<qtdd){
+
+        let carta = document.querySelector(".cards-container");
+            carta.innerHTML=`
+            <div class="card">
+            <img src="imagens/front.png" alt="papagaio-carta oculta" class="parrot-card">
+            </div>
+            ` + carta.innerHTML;
+        i++;
+    }
+}
+
+
+mostrarCartas ();
+
 
